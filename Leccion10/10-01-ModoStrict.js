93972c3ffe8d9515e8572e77c73c893810e3cb24
Matias Antonelli <matiasantonelli6874@gmail.com>
2024-07-24 13:04:54 -0300

    Leccion 10
    modo Strict

---
+++ Leccion10/10-01-ModoStrict.js
@@ -0,0 +1,9 @@
+//"use strict"; //se puede usar al inicio del programa o dentro de una funcion
+
+let x = 10;
+console.log(x);
+miFuncion();
+function miFuncion(){
+    "use strict";
+    let y = 15; 
+}
